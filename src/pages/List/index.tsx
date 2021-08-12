6e702120a53b9fb15b59f6fe3b4b8269e4b3d330
Victor Costelini <costelinivictor@gmail.com>
2021-08-11 21:00:01 -0300

    feat: search bar structure and styling

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,50 +1,55 @@
 import React from 'react';
 import ItemCard from '../../components/ItemCard';
 
-// import { Container } from './styles';
+import { SearchBar } from './styles';
 
 const List: React.FC = () => {
   return (
-    <div className="flex justify-center">
-      <div className="grid grid-cols-3 gap-8 mt-8">
-        <ItemCard
-          imagePath="https://images.punkapi.com/v2/keg.png"
-          title="Buzz"
-          description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
-          className="col-span-1"
-        />
-        <ItemCard
-          imagePath="https://images.punkapi.com/v2/keg.png"
-          title="Buzz"
-          description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
-          className="col-span-1"
-        />
-        <ItemCard
-          imagePath="https://images.punkapi.com/v2/keg.png"
-          title="Buzz"
-          description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
-          className="col-span-1"
-        />
-        <ItemCard
-          imagePath="https://images.punkapi.com/v2/keg.png"
-          title="Buzz"
-          description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
-          className="col-span-1"
-        />
-        <ItemCard
-          imagePath="https://images.punkapi.com/v2/keg.png"
-          title="Buzz"
-          description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
-          className="col-span-1"
-        />
-        <ItemCard
-          imagePath="https://images.punkapi.com/v2/keg.png"
-          title="Buzz"
-          description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
-          className="col-span-1"
-        />
+    <>
+      <SearchBar
+        placeholder="Search"
+      />
+      <div className="flex justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
+          <ItemCard
+            imagePath="https://images.punkapi.com/v2/keg.png"
+            title="Buzz"
+            description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
+            className="col-span-1"
+          />
+          <ItemCard
+            imagePath="https://images.punkapi.com/v2/keg.png"
+            title="Buzz"
+            description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
+            className="col-span-1"
+          />
+          <ItemCard
+            imagePath="https://images.punkapi.com/v2/keg.png"
+            title="Buzz"
+            description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
+            className="col-span-1"
+          />
+          <ItemCard
+            imagePath="https://images.punkapi.com/v2/keg.png"
+            title="Buzz"
+            description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
+            className="col-span-1"
+          />
+          <ItemCard
+            imagePath="https://images.punkapi.com/v2/keg.png"
+            title="Buzz"
+            description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
+            className="col-span-1"
+          />
+          <ItemCard
+            imagePath="https://images.punkapi.com/v2/keg.png"
+            title="Buzz"
+            description="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
+            className="col-span-1"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/pages/List/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const SearchBar = styled.input`
+  display: block;
+  margin: 30px auto 0;
+  max-width: 500px;
+  width: calc(100% - 40px);
+
+  padding: 10px 20px;
+  border: 1px solid #d1d1d1;
+  border-radius: 10px;
+`;
