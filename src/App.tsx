15d0f3388eb2e1ab2eab05225017580b35e2946e
Victor Costelini <costelinivictor@gmail.com>
2021-08-13 09:02:21 -0300

    style: formatted with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { AnimatedSwitch } from 'react-router-transition';
+import { AnimatedSwitch } from "react-router-transition";
 
 import List from "./pages/List";
 import Register from "./pages/Register";
@@ -9,7 +9,7 @@ import Header from "./components/Header";
 
 import AppContext from "./contexts/AppContext";
 
-import { Item } from './types/Item';
+import { Item } from "./types/Item";
 
 function request<TResponse>(
   url: string,
@@ -23,37 +23,35 @@ function request<TResponse>(
 const getItems = async () => {
   const beers = request<Array<Item>>("https://api.punkapi.com/v2/beers");
   return beers;
-}
+};
 
 const App: React.FC = () => {
   const [items, setItems] = useState<Array<Item>>([]);
 
   useEffect(() => {
-    getItems().then(response => setItems(response));
+    getItems().then((response) => setItems(response));
   }, []);
 
   return (
     <AppContext.Provider value={{ items, setItems }}>
       <Router>
-        <div>
-          <Header />
-          <AnimatedSwitch
-            atEnter={{ opacity: 0 }}
-            atLeave={{ opacity: 0 }}
-            atActive={{ opacity: 1 }}
-            className="switch-wrapper"
-          >
-            <Route exact path="/">
-              <List />
-            </Route>
-            <Route exact path="/register">
-              <Register />
-            </Route>
-          </AnimatedSwitch>
-        </div>
+        <Header />
+        <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className="switch-wrapper"
+        >
+          <Route exact path="/">
+            <List />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+        </AnimatedSwitch>
       </Router>
     </AppContext.Provider>
   );
-}
+};
 
 export default App;
