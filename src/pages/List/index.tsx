9d50fd4c5d3ddf67ed8c80b1ecadab4a3e50ad27
Victor Costelini <costelinivictor@gmail.com>
2021-08-13 08:47:33 -0300

    feat: search

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -5,23 +5,40 @@ import { SearchBar } from './styles';
 
 import AppContext from "../../contexts/AppContext";
 import { useContext } from 'react';
+import { useState } from 'react';
 
 const List: React.FC = () => {
   const { items } = useContext(AppContext)!;
+  const [ search, setSearch ] = useState<string>("");
+
   return (
     <>
-      <SearchBar placeholder="Search" />
+      <SearchBar
+        placeholder="Search"
+        onInput={(e) => setSearch(e.currentTarget.value)}
+      />
       <div className="flex justify-center">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
-          {items.map(item => (
-            <ItemCard
-              id={item.id}
-              key={item.id}
-              imagePath={item.image_url}
-              title={item.name}
-              description={item.description}
-              className="col-span-1"
-            />
+          {search
+            ? items.filter((item) => item.name.includes(search) || item.description.includes(search)).map((item) => (
+              <ItemCard
+                id={item.id}
+                key={item.id}
+                imagePath={item.image_url}
+                title={item.name}
+                description={item.description}
+                className="col-span-1"
+              />
+            ))
+            : items.map(item => (
+              <ItemCard
+                id={item.id}
+                key={item.id}
+                imagePath={item.image_url}
+                title={item.name}
+                description={item.description}
+                className="col-span-1"
+              />
           ))}
         </div>
       </div>
